a074f7bcef8f8dd790405a82be800a3ae0756abe
Arin <arinmandal40@gmail.com>
2023-07-30 20:17:11 +0530

    Updated

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 function toggleDiv() {
-  const div = document.getElementById('toggleDiv');
-  if (div.style.display !== 'none') {
-    div.style.display = 'none';
+  const div = document.getElementById("toggleDiv");
+  if (div.style.display !== "none") {
+    div.style.display = "none";
+  } else {
+    div.style.display = "block";
   }
-  else {
-    div.style.display = 'block';
-  }
-};
+}
 
 // const navBtn = document.getElementById('nav-button');
 // navBtn.addEventListener('click', () => toggleDiv());
 
 // Button back to top
-const btnBack = document.getElementById("back")
-btnBack.addEventListener("click", () => topFunction())
-
+const btnBack = document.getElementById("back");
+btnBack.addEventListener("click", () => topFunction());
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 750) {
+  if (
+    document.body.scrollTop > 650 ||
+    document.documentElement.scrollTop > 750
+  ) {
     back.style.display = "block";
   } else {
     back.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+//* When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 
 // Main Carousel
+
